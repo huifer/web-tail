94705eff34f2242a480df9642b69e68cb898aef9
Zen Huifer <huifer97@163.com>
2023-02-06 10:02:44 +0800

    first commit

---
+++ logfile.go
@@ -0,0 +1,130 @@
+package main
+
+import (
+	"bufio"
+	"crypto/md5"
+	"fmt"
+	"io/ioutil"
+	"log"
+	"os"
+	"strings"
+	"time"
+)
+
+const (
+	logConfigFile = "log.li"
+	filePeriod    = 10 * time.Second
+	commentSign   = "#"
+	sep           = "="
+)
+
+var (
+	logLiLastMod time.Time
+
+	logFiles = struct {
+		first string
+		m     map[string]*LogFile
+	}{
+		m: make(map[string]*LogFile),
+	}
+)
+
+// LogFile 日志文件对象
+type LogFile struct {
+	Path  string
+	Alias string
+	// 日志文件路径的md5值
+	PathMd5 string
+}
+
+// ParseLogConfigFileLiPeriod 定时解析日志配置文件
+func ParseLogConfigFileLiPeriod() {
+	for {
+		fi, err := os.Stat(logConfigFile)
+		if err == nil || os.IsExist(err) {
+			curLastMod := fi.ModTime()
+			if fi.ModTime().After(logLiLastMod) {
+				logLiLastMod = curLastMod
+				log.Println("重新加载日志配置文件")
+				InitLogFiles()
+			}
+		} else {
+			log.Printf("日志配置文件[%s]不存在\n", logConfigFile)
+		}
+		fileTicker := time.NewTicker(filePeriod)
+		<-fileTicker.C
+	}
+}
+
+// InitLogFiles 实例化日志文件列表
+func InitLogFiles() {
+	logFiles.m = make(map[string]*LogFile)
+	logFiles.first = ""
+
+	file, err := os.Open(logConfigFile)
+	if err != nil {
+		log.Printf("日志配置文件[%s]打开异常\n", logConfigFile)
+		return
+	}
+	defer file.Close()
+	scanner := bufio.NewScanner(file)
+	for scanner.Scan() {
+		line := scanner.Text()
+		line = strings.TrimSpace(line)
+		fmt.Println(line)
+		if len(line) > 0 && !strings.HasPrefix(line, commentSign) {
+			//li := strings.Split(line, sep)
+			var fs []string
+
+			allFile, _ := GetAllFile(line, fs)
+
+			fmt.Println(allFile)
+			for i, s := range allFile {
+				fmt.Println("i", i)
+				AddLogFile(s, s)
+
+			}
+
+		}
+	}
+}
+
+func GetAllFile(pathname string, s []string) ([]string, error) {
+	rd, err := ioutil.ReadDir(pathname)
+	if err != nil {
+		fmt.Println("read dir fail:", err)
+		return s, err
+	}
+
+	for _, fi := range rd {
+		if !fi.IsDir() {
+			fullName := pathname + "/" + fi.Name()
+			s = append(s, fullName)
+		}
+	}
+	return s, nil
+}
+
+// AddLogFile 向日志文件列表对象添加日志文件
+func AddLogFile(path, alias string) {
+	if FileExists(path) {
+		pathMd5 := fmt.Sprintf("%x", md5.Sum([]byte(path)))
+		logFile := &LogFile{
+			Path:    path,
+			Alias:   alias,
+			PathMd5: pathMd5,
+		}
+		logFiles.m[pathMd5] = logFile
+		if logFiles.first == "" {
+			logFiles.first = pathMd5
+		}
+	} else {
+		log.Printf("日志文件[%s]不存在\n", path)
+	}
+}
+
+// FileExists 判断文件是否存在
+func FileExists(filePath string) bool {
+	_, err := os.Stat(filePath)
+	return err == nil || os.IsExist(err)
+}
